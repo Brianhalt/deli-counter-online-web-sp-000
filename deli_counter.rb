a687b62975c6aeae5aa63d2f7e3619feaf292a26
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 18:01:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |names, i|
-    binding.pry
-    puts "The line is currently: (#{i+1}.) #{names}"
+    # binding.pry
+    puts "The line is currently: #{i+1}. #{names}"
     end
   end
 end
