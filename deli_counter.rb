baef638f68e7f99220a34c76ab1ef229aaf67e7a
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 16:01:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,14 +4,14 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli < 1
     puts "The line is currently empty."
-  elsif 
-  end        
+  elsif
+  end
 end
 
 def take_a_number(katz_deli,name)
 
 end
-  
+
 def now_serving(katz_deli)
 
 end  
