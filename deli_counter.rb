4ce7b6517318767fbe02a9ec7cd6aa9aaef21cd8
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-09 18:08:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(katz_deli,name)
   if katz_deli == []
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{name}"
   end
